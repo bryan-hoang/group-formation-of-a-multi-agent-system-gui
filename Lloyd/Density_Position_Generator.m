8187e4243bfec332fdcc49ce4fc16852f39717d2
Bryan Hoang <Bryan.C.Hoang@gmail.com>
2018-12-02 12:55:05 -0500

    Add realism for battery life

--- Lloyd/Density_Position_Generator.m
+++ Lloyd/Density_Position_Generator.m
@@ -18,55 +18,56 @@ function [Agent_Positions, Density] = Density_Position_Generator(...
                 % Generate Random matrix of non negative density values
                 % Binary Density
                 Density(i,j) = randi([0,1],1);
-                 Density(i,j) = 0;
-                 %% CITY LAYOUT (GRID)
-%                  for w = 1:20:sides*Partition_Number
-%                     if all(i>w && i<w+10)
-%                         Density(i,j) = 1;
-%                     end
-%                     if all(j>w && j<w+10)
-%                         Density(i,j) = 1;
-%                     end
-%                  end
+                Density(i,j) = 0;
+                %% CITY LAYOUT (GRID)
+                %                  for w = 1:20:sides*Partition_Number
+                %                     if all(i>w && i<w+10)
+                %                         Density(i,j) = 1;
+                %                     end
+                %                     if all(j>w && j<w+10)
+                %                         Density(i,j) = 1;
+                %                     end
+                %                  end
                 %% DENSER CIRCLES
-%                  zap1 = sqrt((i-(36))^2+(j-(72))^2);
-%                  portion = randi([0,5],1,1);
-%                  if all(zap1 >= 0 && zap1 <= 17)
-%                     if portion >= 1
-%                          Density(i,j) = 1;
-%                     end
-%                  end
-%                  zap2 = sqrt((i-(72))^2+(j-(18))^2);
-%                  portion = randi([0,5],1,1);
-%                 if all(zap2 >= 0 && zap2 <= 18)
-%                     if portion >= 1
-%                          Density(i,j) = 1;
-%                     end
-%                 end
+                                 zap1 = sqrt((i-(36))^2+(j-(72))^2);
+                                 portion = randi([0,5],1,1);
+                                 if all(zap1 >= 0 && zap1 <= 17)
+                                    if portion >= 1
+                                         Density(i,j) = 1;
+                                    end
+                                 end
+                                 zap2 = sqrt((i-(72))^2+(j-(18))^2);
+                                 portion = randi([0,5],1,1);
+                                if all(zap2 >= 0 && zap2 <= 18)
+                                    if portion >= 1
+                                         Density(i,j) = 1;
+                                    end
+                                end
                 %% FARMLAND ROWS/COLUMNS
-%                 for k = 1:4:sides*Partition_Number
-%                     if all(i>k && i<k+3)
-%                         Density(i,j) = 1;
-%                     end
-%                     if all(j>k && j<k+3)
-%                         Density(i,j) = 1;
-%                     end
-%                 end
+                %                 for k = 1:4:sides*Partition_Number
+                %                     if all(i>k && i<k+3)
+                %                         Density(i,j) = 1;
+                %                     end
+                %                     if all(j>k && j<k+3)
+                %                         Density(i,j) = 1;
+                %                     end
+                %                 end
                 %% CONCENTRIC CIRCLES
-                 for k = 1:4:sides*Partition_Number
-                     zoop = sqrt((i-(0.5*sides*Partition_Number))^2+(j-(0.5*sides*Partition_Number))^2);
-                     if all(zoop > k && zoop < k+1)
-                         Density(i,j) = 1;
-                     end
-                 end
+                for k = 1:4:sides*Partition_Number
+                    zoop = sqrt((i-(0.5*sides*Partition_Number))^2+(j-(0.5*sides*Partition_Number))^2);
+                    if all(zoop > k && zoop < k+1)
+                        Density(i,j) = 1;
+                    end
+                end
+                %% END OF DISTRBIUTIONS
             end
         end
-    %% END OF DISTRBIUTIONS
-    Agent_Positions = zeros(Number_of_Robots,2);
-    
-    for i = 1:Number_of_Robots
-        Agent_Positions(i,1) = rand(1,1)*(sides);
-        Agent_Positions(i,2) = rand(1,1)*(sides);
+        Agent_Positions = zeros(Number_of_Robots,2);
+        
+        for i = 1:Number_of_Robots
+            Agent_Positions(i,1) = rand(1,1)*(sides);
+            Agent_Positions(i,2) = rand(1,1)*(sides);
+        end
     end
     
 end
